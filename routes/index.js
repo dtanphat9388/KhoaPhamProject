d9cef53c37c0c3d3ec215795734db372361a3f29
dtanphat9388 <dtanphat9388@gmail.com>
2017-04-01 18:50:43 +0700

    custom signin and signup form

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 router.get('/', (req, res) => res.render('index'));
 
 router.route('/signin')
-.get((req, res) => res.render('Signin'))
+.get((req, res) => res.render('./routes/signin'))
+.post((req, res) => res.send('recieved a post'))
 
 router.route('/signup')
-.get((req, res) => res.render('Signup'))
+.get((req, res) => res.render('./routes/signup'))
+.post((req, res) => res.send('recieved a post'))
 
 module.exports = router;
