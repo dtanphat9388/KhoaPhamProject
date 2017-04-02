3f5d5bedad8a4d35eb40bbad53e6836da584f7b8
dtanphat9388 <dtanphat9388@gmail.com>
2017-04-02 20:31:39 +0700

    show alert

--- public/js/signin.js
+++ public/js/signin.js
@@ -2,26 +2,22 @@ $(
 
    $("#SigninSubmitBtn").click(function(e){
       e.preventDefault();
+
       const submitBtn = $(this).button('authenticating');
       const username = $("input[name='username']").val();
       const password = $("input[name='password']").val();
 
       $.post('/signin', {username, password}, result => {
-         if(result === null) {
-            console.log(result)
-            $("#alertInfo").appendTo('
-                  <div id="info" class="alert alert-danger alert-dismissible" role="alert">
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                     <span aria-hidden="true">&times;</span>
-                  </button>
-                  <strong>Error: </strong> da co loi xay ra
+         if ( result === null ) {
+            $("#alertInfo").append(`
+               <div class="alert alert-danger alert-dismissible" role="alert">
+                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                 <strong>Warning!</strong> Better check yourself, you're not looking too good.
                </div>
-               '');
-            
+            `);
             submitBtn.button('reset');
          } else {
-            console.log(result)
-            
+            window.location = result.redirect;
          }
       })
    })
--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,16 @@ router.get('/', (req, res) => res.render('index'));
 
 router.route('/signin')
 .get((req, res) => res.render('./routes/signin'))
-.post((req, res) => userModel.getUser(req.body).then( data => {
-   if ( data !== null ) {
-   		console.log(data)
-      // adding jsonwebtoken
-      data.redirect = "/";
-      res.send(data);
-   } else {
-   		console.log(data)
-   		
-      res.send(data)
-   }
-}))
+.post((req, res) => {
+   userModel.getUser(req.body).then( data => {
+      if ( data === null ) {
+         res.json(data);
+      } else {
+         data.redirect = "/";
+         res.json(data);
+      }
+   })
+})
 
 router.route('/signup')
 .get((req, res) => res.render('./routes/signup'))
